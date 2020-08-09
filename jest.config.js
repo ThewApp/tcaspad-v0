484d3c394193eaf818c611d1d08430fab73d2bff
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-08-09 23:31:04 +0700

    Setup test for routes & add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  collectCoverageFrom: ["{components,routes}/**/*.{js,jsx,ts,tsx}"],
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
---
+++ routes/__tests__/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import Index from "../index";
+
+test("render index", async () => {
+  render(<Index />);
+});
